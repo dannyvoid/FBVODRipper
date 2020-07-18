6ceff035a64d2ca1a8a7a90df4bf9c48961c7b30
Danny Void <danielilluminatic@gmail.com>
2020-07-18 03:23:40 -0500

    Update batch.py

--- batch.py
+++ batch.py
@@ -4,9 +4,9 @@ list = "list.txt"
 
 with open(list) as links:
     for line in links:
-        line = line.strip('\n')
-        line = line.strip('\t')
+        line = line.strip("\n")
+        line = line.strip("\t")
 
         command = f"python app.py {line}"
         os.system(command)
-        os.system('cls')
+        os.system("cls")
