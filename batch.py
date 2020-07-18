f6a010d0926db2db60aa084079ba8bf94a533477
Danny Void <danielilluminatic@gmail.com>
2020-07-18 03:45:58 -0500

    don't double download
    let's skip already completed downloads if something happened

--- batch.py
+++ batch.py
@@ -1,12 +1,19 @@
-import os
+import os,os.path
+from os import path
 
 list = "list.txt"
 
 with open(list) as links:
     for line in links:
-        line = line.strip("\n")
-        line = line.strip("\t")
+        line = line.strip('\n')
+        line = line.strip('\t')
 
-        command = f"python app.py {line}"
-        os.system(command)
-        os.system("cls")
+        filename = line.split("/")[-2]
+
+        if path.exists(f"complete/{filename}.mp4"):
+            os.system('cls')
+            continue
+        else:
+            command = f"python app.py {line}"
+            os.system(command)
+            os.system('cls')
