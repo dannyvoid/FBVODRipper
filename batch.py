a2bf8c182bcf2a8f363b6feac34c55cb195badc4
Danny Void <danielilluminatic@gmail.com>
2020-07-18 03:12:03 -0500

    1.0
    Now just a wrapper for youtube-dl

---
+++ batch.py
@@ -0,0 +1,12 @@
+import os
+
+list = "list.txt"
+
+with open(list) as links:
+    for line in links:
+        line = line.strip('\n')
+        line = line.strip('\t')
+
+        command = f"python app.py {line}"
+        os.system(command)
+        os.system(cls)
