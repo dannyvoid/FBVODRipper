3fe9ec0334c8c9eb8541b7e765f95dd57bc764d6
dannyvoid <danielilluminatic@gmail.com>
2020-10-18 05:07:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import os,sys,shutil
-from youtube_dl import YoutubeDL
 
 vods_dir = "vods"
 
@@ -11,43 +10,9 @@ if len(sys.argv) > 1:
 else:
     url = input("Facebook VOD URL: ")
 
-youtube_dl_opts = {}
-video = url
+videoid = url.split("/")[-2]
 
-with YoutubeDL(youtube_dl_opts) as ydl:
-      info_dict = ydl.extract_info(video, download=False)
-      video_url = info_dict.get("url", None)
-      video_id = info_dict.get("id", None)
-      upload_date = info_dict.get("upload_date", None)
-      timestamp = info_dict.get("timestamp", None)
-
-while upload_date is None:
-    try:
-        youtube_dl_opts = {}
-        video = "https://www.facebook.com/arigameplays/videos/334728620980663/"
-        with YoutubeDL(youtube_dl_opts) as ydl:
-            info_dict = ydl.extract_info(video, download=False)
-            video_url = info_dict.get("url", None)
-            video_id = info_dict.get("id", None)
-            upload_date = info_dict.get("upload_date", None)
-            timestamp = info_dict.get("timestamp", None)
-    except:
-        pass
-
-while timestamp is None:
-    try:
-        youtube_dl_opts = {}
-        video = "https://www.facebook.com/arigameplays/videos/334728620980663/"
-        with YoutubeDL(youtube_dl_opts) as ydl:
-            info_dict = ydl.extract_info(video, download=False)
-            video_url = info_dict.get("url", None)
-            video_id = info_dict.get("id", None)
-            upload_date = info_dict.get("upload_date", None)
-            timestamp = info_dict.get("timestamp", None)
-    except:
-        pass
-
-filename = f'{upload_date}_{timestamp}_%(resolution)s.%(ext)s'
+filename = f'%(resolution)s_{videoid}.%(ext)s'
 os.system("title Scraping: "+url)
 
 command = f"youtube-dl -f bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio --merge-output-format mp4 {url} -o {vods_dir}/{filename}"
@@ -55,5 +20,5 @@ os.system(command)
 
 history_file = f"history.txt"
 f = open(history_file, "a+") 
-f.write(f"{upload_date} - {timestamp} - {url}\n")
+f.write(f"{videoid} - {url}\n")
 f.close()
